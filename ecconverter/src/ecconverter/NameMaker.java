81eb1a2fb9161aa2841e065f030a2bdec0b40ab7
hyun1024 <133631600+hyun1024@users.noreply.github.com>
2024-01-31 14:37:57 +0900

    Update NameMaker.java

--- ecconverter/src/ecconverter/NameMaker.java
+++ ecconverter/src/ecconverter/NameMaker.java
@@ -4,18 +4,27 @@ package ecconverter;
 public class NameMaker {
 
 	public String getTargetDirPath() {
-		return Config.BASIC_PATH+"\\";
+		return Config.TARGET_PATH+"\\";
 	}
 	public String createXlsName(String filename) {
-		return Config.BASIC_PATH+"\\"+filename+".xls";
+		return Config.RESULT_PATH+"\\"+filename+".xls";
 	}
 	public String createXlsxName(String filename) {
-		return Config.BASIC_PATH+"\\"+filename+".xlsx";
+		return Config.RESULT_PATH+"\\"+filename+".xlsx";
 	}
 	public String createCsvName(String filename) {
-		return Config.BASIC_PATH+"\\"+filename+".csv";
+		return Config.RESULT_PATH+"\\"+filename+".csv";
+	}
+	public String readCsvName(String filename) {
+		return Config.TARGET_PATH+"\\"+filename+".csv";
+	}
+	public String readXlsName(String filename) {
+		return Config.TARGET_PATH+"\\"+filename+".xls";
+	}
+	public String readXlsxName(String filename) {
+		return Config.TARGET_PATH+"\\"+filename+".xlsx";
 	}
 	public String createNumberingXlsxName(String filename, int count) {
-		return Config.BASIC_PATH+"\\"+filename+"_"+count+".xlsx";
+		return Config.RESULT_PATH+"\\"+filename+"_"+count+".xlsx";
 	}
 }
