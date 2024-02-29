7898b9559ff05d5529c2f95b5c930ab763d7c000
hwPark <haema3043@gmail.com>
2024-02-29 15:12:36 +0900

    create ExcelManager class

---
+++ ecconverter/src/manager/ExcelManager.java
@@ -0,0 +1,26 @@
+package manager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelManager {
+	
+	String extension;
+	
+	public void setExtenstion(String extension) {
+		this.extension = extension;
+	}
+	public Workbook createWorkbookWithInitSheet(FileInputStream fs) throws IOException {
+		switch(extension) {
+			case "xls" : return new HSSFWorkbook(fs);
+			
+			case "xlsx" : return new XSSFWorkbook(fs);
+						  
+			default: return null;
+		}
+	}
+}
